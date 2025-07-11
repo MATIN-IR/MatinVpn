87268eab69913b0cfdbc957865955aa8ddc22bf8
MATIN-IR <tajhostsup@gmail.com>
2025-07-12 00:38:33 +0330

    Update main.go

--- server/main.go
+++ server/main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"matinvpn/auth"
+	"matinvpn/crypto"
+	"matinvpn/obfuscation"
 	"net"
 )
 
+var secretKey = []byte("12345678901234567890123456789012") // 32 bytes AES
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	fmt.Println("New connection from:", conn.RemoteAddr())
-	// در اینجا رمزگشایی، احراز هویت و ... انجام می‌شه
+
+	err := obfuscation.FakeHTTP2Handshake(conn)
+	if err != nil {
+		log.Println("Obfuscation failed:", err)
+		return
+	}
+
+	crypt := crypto.NewAESCrypto(secretKey)
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Println("Error reading:", err)
+		return
+	}
+
+	decrypted, err := crypt.Decrypt(buf[:n])
+	if err != nil {
+		log.Println("Decrypt error:", err)
+		return
+	}
+
+	var username, password string
+	fmt.Sscanf(string(decrypted), "%s|%s", &username, &password)
+
+	if err := auth.Authenticate(username, password); err != nil {
+		io.WriteString(conn, "AUTH_FAILED")
+		return
+	}
+
+	io.WriteString(conn, "AUTH_OK")
+	log.Println("User authenticated:", username)
 }
+
 func main() {
 	listener, err := net.Listen("tcp", ":443")
 	if err != nil {
